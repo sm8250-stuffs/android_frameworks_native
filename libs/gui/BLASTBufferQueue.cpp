f74fa1988003d42e656076570e1868460ae14342
johnmart19 <johnivan19999@gmail.com>
2023-07-02 08:59:12 +0300

    BlastBufferQueque: Import Xiaomi Android 13 BlastBuffer Changes
    Change-Id: I2e87f80ceeb6da76ff8c9c2fca4fdefa03147636

--- libs/gui/BLASTBufferQueue.cpp
+++ libs/gui/BLASTBufferQueue.cpp
@@ -1198,4 +1198,10 @@ void BLASTBufferQueue::setTransactionHangCallback(
     mTransactionHangCallback = callback;
 }
 
+// MIUI ADD: START
+bool BLASTBufferQueue::adjustMaxDequeuedBufferCountForProducer(int count) {
+    return mProducer->adjustMaxDequeuedBufferCount(count) == NO_ERROR;
+}
+// END
+
 } // namespace android
